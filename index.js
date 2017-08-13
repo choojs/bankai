6d3ac1f004368d2a1c9ded6d7a4eea27cadae65c
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-08-13 14:59:13 +0200

    yay, JS compiles!

--- index.js
+++ index.js
@@ -23,20 +23,25 @@ function Bankai (entry, opts) {
   var methods = [
     'manifest',
     'assets',
-    'serviceworker',
+    'serviceWorker',
     'script',
     'style',
     'document'
   ]
+
   this.queue = methods.reduce(function (obj, method) {
     obj[method] = Queue()
     return obj
   }, {})
 
   this.graph = graph()
-  this.graph.on('change', function (name, state) {
-    self.emit('change', name, state)
+  this.graph.on('change', function (nodeName, edgeName, state) {
+    self.emit('change', nodeName, edgeName, state)
+    var eventName = nodeName + ':' + edgeName
+    if (eventName === 'script:bundle') self.queue.script.ready()
+    if (eventName === 'manifest:bundle') self.queue.manifest.ready()
   })
+
   this.graph.on('error', function (err) {
     self.emit('error', err)
   })
@@ -60,7 +65,8 @@ Bankai.prototype.script = function (filename, cb) {
   assert.equal(typeof cb, 'function')
   var self = this
   this.queue.script.add(function () {
-    cb(null, self.graph.data['script:bundle'])
+    var data = self.graph.data.script.bundle
+    cb(null, data)
   })
 }
 
@@ -81,16 +87,16 @@ Bankai.prototype.assets = function (filename, opts) {
 
 function Queue () {
   if (!(this instanceof Queue)) return new Queue()
-  this.ready = false
-  this.arr = []
+  this._ready = false
+  this._arr = []
 }
 
 Queue.prototype.add = function (cb) {
-  if (!this.ready) this.arr.push(cb)
+  if (!this._ready) this._arr.push(cb)
   else cb()
 }
 
 Queue.prototype.ready = function () {
-  this.ready = true
-  while (this.arr.length) this.arr.shift()
+  this._ready = true
+  while (this._arr.length) this._arr.shift()()
 }
