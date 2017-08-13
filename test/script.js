fe13e7e2eb69e4c28913ba4a51e1685a65b57899
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-08-13 15:03:50 +0200

    spruce up test

--- test/script.js
+++ test/script.js
@@ -13,14 +13,14 @@ tape('run a JS pipeline', function (assert) {
   `
 
   var filename = 'js-pipeline-' + (Math.random() * 1e4).toFixed() + '.js'
-  var tmpfile = path.join(os.tmpdir(), filename)
-  fs.writeFileSync(tmpfile, file)
+  var tmpFilename = path.join(os.tmpdir(), filename)
+  fs.writeFileSync(tmpFilename, file)
 
-  var compiler = bankai(tmpfile)
+  var compiler = bankai(tmpFilename)
   compiler.script('bundle.js', function (err, res) {
     assert.error(err, 'no error writing script')
     assert.ok(res, 'output exists')
-    fs.unlinkSync(tmpfile)
+    fs.unlinkSync(tmpFilename)
   })
 })
 
@@ -31,12 +31,12 @@ tape('run a JS pipeline', function (assert) {
   `
 
   var filename = 'js-pipeline-' + (Math.random() * 1e4).toFixed() + '.js'
-  var tmpfile = path.join(os.tmpdir(), filename)
-  fs.writeFileSync(tmpfile, file)
+  var tmpFilename = path.join(os.tmpdir(), filename)
+  fs.writeFileSync(tmpFilename, file)
 
-  var compiler = bankai(tmpfile)
+  var compiler = bankai(tmpFilename)
   compiler.script('bad-bad-not-good.js', function (err, res) {
     assert.ok(err, 'error writing script')
-    fs.unlinkSync(tmpfile)
+    fs.unlinkSync(tmpFilename)
   })
 })
