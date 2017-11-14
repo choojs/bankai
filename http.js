cc02d9cef1800c685e8ec10d0621b8b1f34e0f7d
Dan Flettre <flettre@gmail.com>
2017-11-14 11:49:59 -0600

    reload on document change instead of script (#305)

--- http.js
+++ http.js
@@ -80,7 +80,7 @@ function start (entry, opts) {
     }
     state.files[nodeName] = data
 
-    if (name === 'scripts:bundle') emitter.emit('scripts:bundle', node)
+    if (name === 'documents:index.html') emitter.emit('documents:index.html', node)
     if (name === 'styles:bundle') emitter.emit('styles:bundle', node)
 
     // Only calculate the gzip size if there's a buffer. Apparently zipping
@@ -178,7 +178,7 @@ function start (entry, opts) {
 
   router.route(/\/reload/, function sse (req, res) {
     var connected = true
-    emitter.on('scripts:bundle', reloadScript)
+    emitter.on('documents:index.html', reloadScript)
     emitter.on('styles:bundle', reloadStyle)
     state.sse += 1
     if (!quiet) render()
@@ -202,7 +202,7 @@ function start (entry, opts) {
     function disconnect () {
       clearInterval(interval)
       if (connected) {
-        emitter.removeListener('scripts:bundle', reloadScript)
+        emitter.removeListener('documents:index.html', reloadScript)
         emitter.removeListener('styles:bundle', reloadStyle)
         connected = false
         state.sse -= 1
