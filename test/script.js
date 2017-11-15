275587cf510dae43ee24e80585417a442825d12c
Renée Kooi <renee@kooi.me>
2017-11-15 11:16:26 +0100

    add dynamic bundle test

--- test/script.js
+++ test/script.js
@@ -1,5 +1,6 @@
 var dedent = require('dedent')
 var rimraf = require('rimraf')
+var mkdirp = require('mkdirp')
 var path = require('path')
 var tape = require('tape')
 var fs = require('fs')
@@ -41,5 +42,33 @@ tape('return an error if an incorrect script is selected', function (assert) {
   compiler.scripts('bad-bad-not-good.js', function (err, res) {
     assert.ok(err, 'error writing script')
     rimraf.sync(tmpFilename)
+  })
+})
+
+tape('output multiple bundles if `split-require` is used', function (assert) {
+  assert.plan(2)
+
+  var file = `
+    require('split-require')('./dynamic')
+  `
+  var dynamicFile = `
+    console.log('loaded')
+    module.exports = null
+  `
+
+  var tmpDirname = path.join(__dirname, '../tmp', 'js-pipeline-' + (Math.random() * 1e4).toFixed())
+  mkdirp.sync(tmpDirname)
+  fs.writeFileSync(path.join(tmpDirname, 'app.js'), file)
+  fs.writeFileSync(path.join(tmpDirname, 'dynamic.js'), dynamicFile)
+
+  var compiler = bankai(path.join(tmpDirname, 'app.js'), { watch: false })
+  compiler.scripts('bundle.js', function (err, res) {
+    assert.error(err, 'error writing main bundle')
+    var dynamicName = /bundle-\d+\.js/.exec(res.buffer.toString('utf-8'))
+    compiler.scripts(dynamicName[0], function (err, res) {
+      assert.error(err, 'error writing dynamic bundle')
+
+      rimraf.sync(tmpDirname)
+    })
   })
 })
