79d2aad6b9a135f972265e6edf9019524800aa49
Yerko Palma <yerko.palma@usach.cl>
2018-01-11 17:25:44 -0300

    Support dots in file names and ignore query strings for static files (#373)
    * support dots for js and css files in bankai/http
    
    * ignore querystrings in assets route
    
    * standard
    
    * ignore querystrings in css and js files

--- http.js
+++ http.js
@@ -117,7 +117,7 @@ function start (entry, opts) {
     })
   })
 
-  router.route(/\/([a-zA-Z0-9-_]+)\.js$/, function (req, res, params) {
+  router.route(/\/([a-zA-Z0-9-_.]+)\.js\?{0,1}[\w&=]*$/, function (req, res, params) {
     var name = params[1]
     compiler.scripts(name, function (err, node) {
       if (err) {
@@ -129,7 +129,7 @@ function start (entry, opts) {
     })
   })
 
-  router.route(/\/([a-zA-Z0-9-_]+)\.css$/, function (req, res, params) {
+  router.route(/\/([a-zA-Z0-9-_.]+)\.css\?{0,1}[\w&=]*$/, function (req, res, params) {
     var name = params[1]
     compiler.styles(name, function (err, node) {
       if (err) {
@@ -164,7 +164,7 @@ function start (entry, opts) {
     })
   })
 
-  router.route(/^\/assets\/(.*)$/, function (req, res, params) {
+  router.route(/^\/assets\/([a-zA-Z0-9-_.]*)\?{0,1}[\w&=]*$/, function (req, res, params) {
     var prefix = 'assets' // TODO: also accept 'content'
     var name = prefix + '/' + params[1]
     compiler.assets(name, function (err, filename) {
