306ccf94008a9b24c4924e463ae59d6cdfb64e63
Bendik Aagaard Lynghaug <bendik.lynghaug@gmail.com>
2018-04-05 10:40:44 -0400

    no comment either

--- ssr/choo.js
+++ ssr/choo.js
@@ -69,7 +69,6 @@ module.exports.render = function (app, route, cb) {
     try {
       app.toString(route, state)
     } catch (err) {
-      // logging full stack trace to aid with debugging, sorry ui.
       return reject(err)
     }
     resolve(state._experimental_prefetch)
